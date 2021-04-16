be98d3a8111676ebe2180ccd2f69bf0c2bcaa962
guillaumeHaon <>
2021-04-16 14:56:19 +0000

    modif' Achetables.h

---
+++ Achetables.h
@@ -0,0 +1,22 @@
+#ifndef __Case_h
+#define __Case_h
+
+#include "case.h"
+#include "Joueur.h"
+#include "string.h"
+
+using namespace std;
+
+class Achetables{ //classe fille
+protected:
+  int prix; //prix de la case
+  Joueur propriétaire;
+  bool achetable; //si case déjà achetée ou pas
+
+public:
+  Achetables();
+  acheter(Joueur);
+  virtual calculLoyer(Achetables a);
+};
+
+#endif
