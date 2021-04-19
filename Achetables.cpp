3239f16bdfb75c2791b3a6c027158374ecf28b71
ThomasDebeire <>
2021-04-19 10:16:12 +0000

    Update Achetables.cpp

--- Achetables.cpp
+++ Achetables.cpp
@@ -1,1 +1,10 @@
 #include "Achetables.h"
+#include "Joueur.h"
+
+ // constructeur
+  Achetables::Achetables()//initialisation de base
+  {
+   this->prix = 9999;
+   this->proprietaire = ;
+   this->achetable = 1;
+  }
