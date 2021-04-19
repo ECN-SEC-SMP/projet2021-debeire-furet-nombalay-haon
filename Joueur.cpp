2e32bca450bc3b6a609ffa22b84b6655cab363c6
Manu Frt <>
2021-04-19 08:39:08 +0000

    Correction Joueur cpp et h

--- Joueur.cpp
+++ Joueur.cpp
@@ -1,1 +1,39 @@
-#include "Joueurs.h"
+#include "Joueur.h"
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ // accesseurs en lecture
+ string Joueur::getNom() { return nomJoueur; }
+ int Joueur::getFortune() { return fortune; }
+ int Joueur::getPosition() { return position; }
+ 
+ // accesseurs en ecriture
+ void Joueur::setNom(string nomJoueur) { this->nomJoueur = nomJoueur; }
+ void Joueur::setFortune(int fortune) { this->fortune = fortune; }
+ void Joueur::setPosition(int position) { this->position = position; }
+ 
+ // constructeur
+  Joueur::Joueur(string nomJoueur)//initialisation de base
+  {
+   this->nomJoueur = nomJoueur;
+   this->fortune = 100000;
+   this->position = 0;
+  }
+ 
+ bool Joueur::Paiement(Joueur donne,Joueur recoit, int somme)//Paiement à un autre joueur
+ {
+   if (somme<donne.getFortune())
+   {
+     int fortune_donne = (donne.getFortune()-somme);
+     donne.setFortune(fortune_donne);
+     int fortune_recoit = (recoit.getFortune()+somme);
+     recoit.setFortune(fortune_recoit);
+     return true;
+   }
+   else
+   {
+     return false;
+   }
+ }
+};
--- Joueur.h
+++ Joueur.h
@@ -4,54 +4,25 @@
 using namespace std;
  
 class Joueur {
- private:
- string nomJoueur;
- int fortune;
- int position;
- int nbGares;
+  private:
+  string nomJoueur;
+  int fortune;
+  int position;
+  int nbGares;
+
+  public:
+  // accesseurs en lecture
+  string getNom();
+  int getFortune();
+  int getPosition();
+
+  // accesseurs en ecriture
+  void setNom(string nomJoueur); 
+  void setFortune(int fortune); 
+  void setPosition(int position); 
  
- public:
- // accesseurs en lecture
- string getNom() { return nomJoueur; }
- int getFortune() { return fortune; }
- int getPosition() { return position; }
- int getNbGares() { return nbGares;}
- 
- // accesseurs en ecriture
- void setNom(string nomJoueur) { this->nomJoueur = nomJoueur; }
- void setFortune(int fortune) { this->fortune = fortune; }
- void setPosition(int position) { this->position = position; }
- void setNbGares(int nbGares) { this->nbGares = nbGares;}
- 
- // constructeur
- Joueur(string nomJoueur, int fortune, int position, int nbGares)
- {
-   this->nomJoueur = nomJoueur;
-   this->fortune = fortune;
-   this->position = position;
-   this->nbGares = nbGares;
- }
-  void initJoueur(string nomJoueur)//initialisation de base
- {
-   this->nomJoueur = nomJoueur;
-   this->fortune = 100000;
-   this->position = 0;
-   this->nbGares = 0;
- }
- 
- bool Paiement(Joueur donne,Joueur recoit, int somme)//Paiement à un autre joueur
- {
-   if (somme<getFortune())
-   {
-     int fortune_donne = (donne.getFortune()-somme);
-     donne.setFortune(fortune_donne);
-     int fortune_recoit = (recoit.getFortune()+somme);
-     recoit.setFortune(fortune_recoit);
-     return true;
-   }
-   else
-   {
-     return false;
-   }
- }
+  // constructeur
+  Joueur(string nomJoueur);//initialisation de base
+
+  bool Paiement(Joueur donne,Joueur recoit, int somme);//Paiement à un autre joueur
 };
